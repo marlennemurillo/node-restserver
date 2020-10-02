93a5c78b90eae4a17ac93552d20793bf5585fc4d
MarleneMurillo <marlennemurillo0@gmail.com>
2020-10-02 00:50:12 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafenode:dEIvBTn0k1T9rp1p@cluster0.sdf9q.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
